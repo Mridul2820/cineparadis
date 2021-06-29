de80c1bfec44d343afa116c6febf1aeb288b3be4
mridul2820 <immridulpanda2820@gmail.com>
2021-06-29 10:23:59 +0530

    PWA creation

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import FirebaseContext from './context/firebase'
 import { firebase, FieldValue } from './lib/firebase'
+import * as serviceWorkerRegistration from "./serviceWorkerRegistration";
 
 ReactDOM.render(
     <FirebaseContext.Provider value={{ firebase, FieldValue }}>
@@ -10,3 +11,5 @@ ReactDOM.render(
     </FirebaseContext.Provider>,
     document.getElementById('root')
 );
+
+serviceWorkerRegistration.register();
