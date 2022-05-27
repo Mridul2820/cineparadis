ed64e10a1a0015d6cf5a36c1688e7e330e103590
Mridul2820 <immridulpanda2820@gmail.com>
2022-05-27 22:06:50 +0530

    erroes fix

--- webpack.config.js
+++ webpack.config.js
@@ -2,9 +2,10 @@ module.exports = {
   module: {
     rules: [
       {
-        test: /\.js$/,
         enforce: 'pre',
-        use: ['source-map-loader'],
+        test: /\.js$/,
+        loader: 'source-map-loader',
+        exclude: /node_modules\/@firebase\/auth/,
       },
     ],
   },
