28c56037701f85032c43d46d9c0a5f52a29d01e0
Mridul2820 <immridulpanda2820@gmail.com>
2022-05-29 17:31:38 +0530

    vote color update

--- src/helpers/voteColor.js
+++ src/helpers/voteColor.js
@@ -1,8 +1,10 @@
 const voteColor = (voteAvg) => {
-  if (voteAvg > 7) {
+  if (voteAvg >= 8) {
     return '#388e3c';
-  } else if (voteAvg < 7 && voteAvg > 4) {
+  } else if (voteAvg < 8 && voteAvg >= 6) {
     return '#303f9f';
+  } else if (voteAvg < 6 && voteAvg >= 3.5) {
+    return '#ef6c00';
   } else if (voteAvg === 0) {
     return '#666';
   } else {
