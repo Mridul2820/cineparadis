a014f6fc126b874264cb3efa3ff2f342310ecac6
Mridul2820 <immridulpanda2820@gmail.com>
2022-05-31 00:25:32 +0530

    recomande page  issue fix of link

--- src/components/details/Recomamded.js
+++ src/components/details/Recomamded.js
@@ -6,7 +6,7 @@ const Recommended = ({ recommended }) => {
     <section className="tab-section">
       <h2 className="detail-tab-title">More Like This</h2>
       {recommended.length > 0 ? (
-        <ContentGrid items={recommended} recommended />
+        <ContentGrid items={recommended} samepage />
       ) : (
         <p className="text-center text-slate-500 mt-3">
           Oops, no recommendations found
