c2e5a218e62e38452d0bc627302f6374fefedf83
Mridul2820 <immridulpanda2820@gmail.com>
2022-05-31 00:06:37 +0530

    meta tags ssr

---
+++ src/meta/server.js
@@ -0,0 +1,42 @@
+import express from 'express';
+import React from 'react';
+import ReactDOMServer from 'react-dom/server';
+import DocumentMeta from './serverlib';
+import Root from '../App';
+import * as ROUTES from '../constants/routes';
+
+// Pages
+import TrendingPage from '../pages/topic/Trending';
+
+const server = express();
+
+function ssr(res, content) {
+  const meta = DocumentMeta.renderAsHTML();
+
+  res.send(`<html>
+    <head>
+      ${meta}
+    </head>
+    <body>
+      <div id='root'>${content}</div>
+    </body>
+    <script src="/bundle.js"></script>
+  </html>`);
+}
+
+server.get('/', (req, res) => {
+  ssr(res, ReactDOMServer.renderToString(<Root />));
+});
+
+server.get(ROUTES.Trending, (req, res) => {
+  ssr(
+    res,
+    ReactDOMServer.renderToString(
+      <Root>
+        <TrendingPage />
+      </Root>
+    )
+  );
+});
+
+server.listen(9001);
