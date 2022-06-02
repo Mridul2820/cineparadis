58105cdda1115ea688bf2bddb9c76d65ce039901
Mridul2820 <immridulpanda2820@gmail.com>
2022-06-02 10:56:35 +0530

    paginate person movies

--- src/components/person/PersonMovies.js
+++ src/components/person/PersonMovies.js
@@ -1,10 +1,25 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import ContentGrid from '../widget/ContentGrid';
+import Paginate from '../widget/Paginate';
 
 const PersonMovies = ({ credits }) => {
+  const itemPerPage = 12;
+  const [items, setItems] = useState();
+  const [page, setPage] = useState(1);
+
+  const numOfPages = Math.ceil(credits.cast.length / itemPerPage);
+
+  useEffect(() => {
+    setItems(credits.cast.slice((page - 1) * itemPerPage, itemPerPage * page));
+  }, [page]);
+
   return (
-    <div>
-      <ContentGrid items={credits.cast} nohover showCredit />
+    <div className="mb-2">
+      <ContentGrid items={items} nohover showCredit />
+
+      {numOfPages > 1 && credits.cast.length > 0 && (
+        <Paginate setPage={setPage} numOfPages={numOfPages} />
+      )}
     </div>
   );
 };
