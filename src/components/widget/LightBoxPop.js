e606b8e8bff963172869ebe9088da2da3aedde8f
Mridul2820 <immridulpanda2820@gmail.com>
2022-06-03 19:13:07 +0530

    image light box prev button issue

--- src/components/widget/LightBoxPop.js
+++ src/components/widget/LightBoxPop.js
@@ -6,16 +6,16 @@ const LightBoxPop = ({ imageArray, photoIndex, setPhotoIndex, setIsOpen }) => {
   return (
     <Lightbox
       mainSrc={imageArray[photoIndex]}
+      onCloseRequest={() => setIsOpen(false)}
       nextSrc={imageArray[(photoIndex + 1) % imageArray.length]}
       prevSrc={
         imageArray[(photoIndex + imageArray.length - 1) % imageArray.length]
       }
-      onCloseRequest={() => setIsOpen(false)}
-      onMovePrevRequest={() =>
-        setPhotoIndex(photoIndex + imageArray.length - 1) % imageArray.length
-      }
       onMoveNextRequest={() =>
         setPhotoIndex((photoIndex + 1) % imageArray.length)
+      }
+      onMovePrevRequest={() =>
+        setPhotoIndex((photoIndex + imageArray.length - 1) % imageArray.length)
       }
     />
   );
