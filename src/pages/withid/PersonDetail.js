33391aec160d28d218569a521434daf5d3e21025
Mridul2820 <immridulpanda2820@gmail.com>
2022-06-03 14:06:54 +0530

    Person Detail Seo add

--- src/pages/withid/PersonDetail.js
+++ src/pages/withid/PersonDetail.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 import React, { useEffect, useState } from 'react';
 import Loader from 'react-loader-spinner';
+import DocumentMeta from 'react-document-meta';
 import { useParams } from 'react-router-dom';
 import DetailCard from '../../components/person/DetailCard';
 import PersonIntro from '../../components/person/PersonIntro';
@@ -8,6 +9,7 @@ import PresonTabs from '../../components/person/PresonTabs';
 import { API_URL, BASE_URL } from '../../constants/constant';
 
 import { Container } from '../../styles/Styles';
+import { img500 } from '../../helpers/config';
 
 const detailURL = `${API_URL}/`;
 const apiKey = `api_key=${process.env.REACT_APP_TMDB}`;
@@ -33,6 +35,26 @@ const PersonDetail = () => {
     // eslint-disable-next-line
   }, []);
 
+  const getTitle = creditData?.name ? creditData.name + ' - ' : ' ';
+  const getSlug = `person/${id}`;
+  const getOgImg = creditData?.profile_path
+    ? `${img500}${creditData.profile_path}`
+    : '';
+
+  const meta = {
+    title: `${getTitle} CineParadis`,
+    description: `Discover all destails, biography, Social Links, Movies and Secries and Images of ${creditData?.name} - CineParadis`,
+    canonical: `${BASE_URL}${getSlug}`,
+    meta: {
+      property: {
+        'og:image': getOgImg,
+        'og:title': `${getTitle} CineParadis`,
+        'og:description': `Discover all destails, biography, Social Links, Movies and Secries and Images of ${creditData?.name} - CineParadis`,
+        'og:url': `${BASE_URL}/${getSlug}`,
+      },
+    },
+  };
+
   if (loading) {
     return (
       <Container className="flex flex-col justify-center items-center w-full">
@@ -48,14 +70,16 @@ const PersonDetail = () => {
   }
 
   return (
-    <Container>
-      {creditData && <PersonIntro creditData={creditData} />}
+    <DocumentMeta {...meta} extend>
+      <Container>
+        {creditData && <PersonIntro creditData={creditData} />}
 
-      <div className="flex flex-col md:flex-row gap-4 items-start mt-4">
-        {creditData && <DetailCard creditData={creditData} />}
-        {creditData && <PresonTabs creditData={creditData} />}
-      </div>
-    </Container>
+        <div className="flex flex-col md:flex-row gap-4 items-start mt-4">
+          {creditData && <DetailCard creditData={creditData} />}
+          {creditData && <PresonTabs creditData={creditData} />}
+        </div>
+      </Container>
+    </DocumentMeta>
   );
 };
 
