82c482e69b0c344f4932c6d0b5bacc3d5b93c9bc
Mridul2820 <immridulpanda2820@gmail.com>
2022-06-10 20:41:51 +0530

    normal export add for formatTime

--- src/helpers/formatTime.js
+++ src/helpers/formatTime.js
@@ -1,8 +1,6 @@
-const formatTime = (time) => {
+export const formatTime = (time) => {
   const hours = Math.floor(time / 60);
   const minutes = time % 60;
 
   return `${hours}h ${minutes}m`;
 };
-
-export default formatTime;
